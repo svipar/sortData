9061b8127c7d94ae1d1a02d22515629426febb82
sviatoslav <sv10ps@gmail.com>
2020-11-04 13:51:32 +0200

    insert sort added

--- sortArr/main.c
+++ sortArr/main.c
@@ -3,17 +3,33 @@ const int sz = 20;
 void initArr(double *, int);
 void outArr(double *, int);
 void sortArr(double *, int);
+void insertSortArr(double *, int);
 
 int main()
 {
     double arr[sz];
     initArr(arr,sz);
     outArr(arr,sz);
-    sortArr(arr,sz);
+    //sortArr(arr,sz);
+    insertSortArr(arr,sz);
     outArr(arr,sz);
     printf("Hello World!\n");
     return 0;
 }
+
+void insertSortArr(double * pd, int s){
+    double temp;
+    for(int i=0;i<s-1; i++)
+    {
+        for(int j=i+1; (pd[j] > pd[j-1]) && (j>0) ; j--)
+        {
+            temp = pd[j];
+            pd[j]=pd[j-1];
+            pd[j-1]=temp;
+        }
+    }
+
+};
 
 void initArr(double *pd, int s){
     for(int i=0;i<s;i++)
