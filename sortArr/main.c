93edcc6a896ad59b29e003da12ce832edf2873a4
sviatoslav <sv10ps@gmail.com>
2020-11-04 14:23:20 +0200

    sort find

--- sortArr/main.c
+++ sortArr/main.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 const int sz = 20;
 void initArr(double *, int);
+void initArrRand(double *, int);
 void outArr(double *, int);
 void sortArr(double *, int);
 void insertSortArr(double *, int);
+void findSortArr(double *, int);
 
 int main()
 {
     double arr[sz];
-    initArr(arr,sz);
+    initArrRand(arr,sz);
     outArr(arr,sz);
     //sortArr(arr,sz);
-    insertSortArr(arr,sz);
+    //insertSortArr(arr,sz);
+    findSortArr(arr,sz);
     outArr(arr,sz);
     printf("Hello World!\n");
     return 0;
 }
+
+void initArrRand(double *pd, int s){
+    for(int i=0;i<s;i++)
+        pd[i]=rand()%21;
+
+};
+
+void findSortArr(double *pd, int s){
+    double temp;
+    int needPos;
+    for(int i=0;i<s-1;i++)
+    {
+        temp = pd[i];
+        needPos = i;
+        for(int j=i+1;j<s;j++)
+        {
+            if(temp < pd[j])
+            {
+                temp = pd[j];
+                needPos=j;
+            }
+        }
+        pd[needPos]=pd[i];
+        pd[i]=temp;
+    }
+
+};
+
 
 void insertSortArr(double * pd, int s){
     double temp;
